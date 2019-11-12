7cc84bf72d8d45099921d207395a3bbc17d8f13e
garden1204 <a01062374285@gmail.com>
2019-11-12 19:05:20 +0900

    todo

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+import "./App.css"
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      value: ""
+    };
+  }
+
+  onClickAdd = e => {
+    const items = this.state.items;
+    items.push(this.state.value);
+    this.setState({
+      items
+    });
+  };
+
+  onChange = e => {
+    this.setState({ value: e.target.value });
+  };
+
+  render() {
+    return (
+      <>
+        <input type="text" onChange={this.onChange} />
+        <button onClick={this.onClickAdd}>추가</button>
+        {this.state.items.map((value, idx) => (
+          <TodoItem key={idx} value={value} />
+        ))}
+      </>
+    );
+  }
+}
+export default App;
